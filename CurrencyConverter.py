f73a30682e053288587ef3fa4e7bc27ab604d9b6
Hejdan <herrm0rris@hotmail.com>
2020-08-18 09:39:56 +0200

    Amount now float

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 4:
     convertFrom = input("Choose which currency to convert from: ")
     convertTo = input("Choose which currency to convert to: ")
     amount = input("What amount from start currency: ")
-    converted = rates.convert(convertFrom, convertTo, int(amount))
+    converted = rates.convert(convertFrom, convertTo, float(amount))
     print('{} {}'.format(converted, convertTo))
 elif len(sys.argv) > 4:
     print("Too many arguments")
@@ -17,6 +17,6 @@ else:
     convertFrom = sys.argv[1]
     convertTo = sys.argv[2]
     amount = sys.argv[3]
-    converted = rates.convert(convertFrom, convertTo, int(amount))
+    converted = rates.convert(convertFrom, convertTo, float(amount))
     print('{} {}'.format(converted, convertTo))
 
